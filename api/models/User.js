4c3fbd678b4af439c197d24715d233ba692ecae3
Anna Kleszcz <annakleszcz@onet.pl>
2017-12-14 17:53:00 +0100

    Password hashing removed

--- api/models/User.js
+++ api/models/User.js
@@ -1,43 +1,11 @@
 'use strict'
 var mongoose = require('mongoose');
-var bcrypt = require('bcrypt');
-var SALT_WORK_FACTOR = 10;
 var Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
-  login: {type: String, unique: true},
+  login: String,
   password: String,
   admin: Boolean
 });
-
-//https://stackoverflow.com/questions/14588032/mongoose-password-hashing
-UserSchema.pre('save', function(next) {
-    var user = this;
-
-    if (!user.isModified('password')) return next();
-
-    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
-        if (err) {
-          return next(err);
-        }
-
-        bcrypt.hash(user.password, salt, function(err, hash) {
-            if (err) {
-              return next(err);
-            }
-            user.password = hash;
-            next();
-        });
-    });
-});
-
-UserSchema.methods.comparePassword = function(candidatePassword, callback) {
-    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
-        if (err) {
-          return callback(err);
-        }
-        callback(null, isMatch);
-    });
-};
 
 module.exports = mongoose.model('User', UserSchema);
