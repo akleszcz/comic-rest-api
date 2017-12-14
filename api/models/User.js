c955da691d80a51daa8660064ea15f5381127457
Anna Kleszcz <annakleszcz@onet.pl>
2017-12-14 18:10:23 +0100

    Password hashing added to user model

--- api/models/User.js
+++ api/models/User.js
@@ -5,9 +5,39 @@ var SALT_WORK_FACTOR = 10;
 var Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
-  login: String,
+  login: {type: String, unique: true},
   password: String,
   admin: Boolean
 });
+
+//https://stackoverflow.com/questions/14588032/mongoose-password-hashing
+UserSchema.pre('save', function(next) {
+    var user = this;
+
+    if (!user.isModified('password')) return next();
+
+    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+        if (err) {
+          return next(err);
+        }
+
+        bcrypt.hash(user.password, salt, null, function(err, hash) {
+            if (err) {
+              return next(err);
+            }
+            user.password = hash;
+            next();
+        });
+    });
+});
+
+UserSchema.methods.comparePassword = function(candidatePassword, callback) {
+    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+        if (err) {
+          return callback(err);
+        }
+        callback(null, isMatch);
+    });
+};
 
 module.exports = mongoose.model('User', UserSchema);
