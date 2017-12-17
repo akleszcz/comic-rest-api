2fb4eecaafc633a4a54d011b67b867bc8f94bd78
Anna Kleszcz <annakleszcz@onet.pl>
2017-12-17 15:43:10 +0100

    node_modules folder added to .gitignore

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
 
-    'secret': process.env.secret,
-    'database': process.env.connectionstring
+    'secret': 'testsecret',
+    'database': 'mongodb://mongo:mongo@ds157185.mlab.com:57185/comic'
 
 };
